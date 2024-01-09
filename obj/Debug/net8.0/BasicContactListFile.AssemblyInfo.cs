d337b94d719e58657e1c9decaaba97be3d45a991
Tahir Bastu Abiodun <absullahabeyking@gmail.com>
2024-01-09 14:08:40 +0100

    Update .gitignore to ignore obj and bin directories

--- obj/Debug/net8.0/BasicContactListFile.AssemblyInfo.cs
+++ obj/Debug/net8.0/BasicContactListFile.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BasicContactListFile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddf01bf407abfa57d556e799311a54b72242f651")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BasicContactListFile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BasicContactListFile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
